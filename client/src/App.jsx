6bebcebd70b4fbef291444f797a29a2b8bd637dd
Sreehari <sreehari1672000@gmail.com>
2023-10-01 03:11:55 +0530

    UI Final Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Reaction from "./components/Reaction";
 import { motion as m } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
+// import Voice from "./components/Voice";
 
 function App() {
   const [leftWidth, setLeftWidth] = useState(0);
@@ -18,14 +19,26 @@ function App() {
   return (
     <div className='bg-gradient-to-tr from-blue-700 via-teal-600 to-slate-400 h-screen w-screen flex flex-col'>
       <div className='flex flex-col justify-center'>
+        <m.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 3 }}>
+          <img
+            src='/monitus_branding.svg'
+            alt='Monitus Branding'
+            className='h-32 w-1/4  mx-auto mt-16'
+          />
+        </m.div>
+
         <div className='justify-center flex pt-32'>
           <m.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 3 }}
-            className='bg-[#ffffff4d] justify-around text-black rounded-2xl p-8 font-bold flex gap-8 md:gap-10 lg:gap-16 text-lg shadow-xl  max-h-32 sm:max-w-xl md:max-w-[55rem] lg:max-w-6xl xl:max-w-full'>
+            className='bg-[#ffffffaf] justify-around text-black rounded-2xl p-8 font-bold flex gap-8 md:gap-10 lg:gap-16 text-lg shadow-xl  max-h-32 sm:max-w-xl md:max-w-[55rem] lg:max-w-6xl xl:max-w-full'>
             <Dropbox />
             <Prompt />
+            {/* <Voice /> */}
             <FontAwesomeIcon
               className='mt-4 hover:scale-110 hover:transition hover:duration-500 sm:text-2xl sm:pt-3 md:text-3xl lg:pt-2'
               // size='2x'
